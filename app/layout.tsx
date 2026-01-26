b81a9fd3e305eff16d682908ed31d3ff58169e1a
Sam <loudbooked@gmail.com>
2026-01-25 23:07:12 -0500

    feat: add opengraph dimensions

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,6 +60,8 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="og:image" content="https://thesamgordon.com/opengraph-image.png" />
+        <meta name="og:image:width" content="3840" />
+        <meta name="og:image:height" content="2160" />
       </head>
       <body className={`${inter.variable} ${overusedGroteske.className}`}>{children}</body>
     </html>
